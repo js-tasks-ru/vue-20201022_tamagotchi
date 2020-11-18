63e260d0c3064b264b6ec86ba22fa47506c75257
Tamagotchi9 <42389874+Tamagotchi9@users.noreply.github.com>
2020-11-18 18:52:51 +0200

    Solution of first and second tasks from 03-vue-cli

--- 03-vue-cli/01-CounterButton-vue/components/CounterButton.vue
+++ 03-vue-cli/01-CounterButton-vue/components/CounterButton.vue
@@ -1,7 +1,27 @@
-<template><div></div></template>
+<template>
+  <div>
+    <button type="button" @click="addCounter">{{ count }}</button>
+  </div>
+</template>
 
 <script>
-export default {};
+export default {
+  model: {
+    prop: 'count',
+    event: 'increment',
+  },
+  props: {
+    count: {
+      type: Number,
+      default: 0,
+    },
+  },
+  methods: {
+    addCounter() {
+      this.$emit('increment', this.count + 1);
+    },
+  },
+};
 </script>
 
 <style></style>
