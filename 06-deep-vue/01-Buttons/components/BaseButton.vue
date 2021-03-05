9b75d33a0958ef129ff826b45678cb6068fffd8e
Tamagotchi9 <42389874+Tamagotchi9@users.noreply.github.com>
2021-03-05 17:52:47 +0200

    Solution of 06-deep-vue 01-Buttons

--- 06-deep-vue/01-Buttons/components/BaseButton.vue
+++ 06-deep-vue/01-Buttons/components/BaseButton.vue
@@ -1,10 +1,38 @@
 <template>
-  <button class="button"></button>
+  <button
+    :is="tag"
+    class="button"
+    :class="{ button_block: block }"
+    :href="href"
+    :target="target"
+    v-on="$listeners"
+  >
+    <slot name="default"></slot>
+  </button>
 </template>
 
 <script>
 export default {
   name: 'BaseButton',
+  props: {
+    block: {
+      type: Boolean,
+    },
+    tag: {
+      type: String,
+      default: 'button',
+      validator: function (value) {
+        return ['button', 'a', 'router-link'].indexOf(value) !== -1;
+      },
+    },
+  },
+
+  data() {
+    return {
+      href: '',
+      target: '',
+    };
+  },
 };
 </script>
 
