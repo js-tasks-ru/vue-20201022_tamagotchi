d0309cfa67c9a73d219961d1fbb2ccc88e8bfe76
javascriptru <67475495+jsru-1@users.noreply.github.com>
2022-06-17 22:19:50 +0300

    Added solution for task: Transitions-2 | VNodes

--- 06-deep-vue/08-Transitions-2/components/FadeTransitionGroup.vue
+++ 06-deep-vue/08-Transitions-2/components/FadeTransitionGroup.vue
@@ -1,6 +1,70 @@
 <script>
+/**
+ * Клонирует узел
+ * Код из исходников Vue: https://github.com/vuejs/vue/blob/dev/src/core/vdom/vnode.js#L89
+ * @param {VNode} vnode
+ * @returns {VNode} клонированный узел
+ */
+function cloneVNode(vnode) {
+  // Мы не можем импортировать VMode из пакета Vue
+  // Он не экспортируется в пакете,
+  // а исходники требуют сборки из Flow
+  // Но мы можем получить конструктор из прототипа экземпляра
+  const VNode = vnode.__proto__.constructor;
+  const cloned = new VNode(
+    vnode.tag,
+    vnode.data,
+    // #7975
+    // clone children array to avoid mutating original in case of cloning
+    // a child.
+    vnode.children && vnode.children.slice(),
+    vnode.text,
+    vnode.elm,
+    vnode.context,
+    vnode.componentOptions,
+    vnode.asyncFactory,
+  );
+  cloned.ns = vnode.ns;
+  cloned.isStatic = vnode.isStatic;
+  cloned.key = vnode.key;
+  cloned.isComment = vnode.isComment;
+  cloned.fnContext = vnode.fnContext;
+  cloned.fnOptions = vnode.fnOptions;
+  cloned.fnScopeId = vnode.fnScopeId;
+  cloned.asyncMeta = vnode.asyncMeta;
+  cloned.isCloned = true;
+  return cloned;
+}
+
 export default {
   name: 'FadeTransitionGroup',
+
+  inheritAttrs: false,
+
+  render(h) {
+    return h(
+      'transition-group',
+      {
+        on: this.$listeners,
+        attrs: this.$attrs,
+        props: {
+          name: 'fade-list',
+        },
+        class: 'fade-list',
+      },
+      this.$slots.default.map((vnode) => {
+        const clonedNode = cloneVNode(vnode);
+        if (clonedNode.data.staticClass) {
+          if (!/\bfade-list-item\b/.test(clonedNode.data.staticClass)) {
+            clonedNode.data.staticClass += ' fade-list-item';
+          }
+        } else {
+          clonedNode.data.staticClass = 'fade-list-item';
+        }
+        return clonedNode;
+      }),
+    );
+  },
 };
 </script>
 
